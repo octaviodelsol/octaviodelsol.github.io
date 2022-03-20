e506a11520d3ca7ccbb893c39a503e44bb8b8a3c
octa <octaviodelsol15@gmail.com>
2022-03-20 17:16:27 -0400

    more files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'octaviodelsol.github.io';
+
+  goToSheet() {
+    window.location.href = 'https://docs.google.com/forms/d/1iE6H_ipPmq3RP1fiuRow6G6roI2WjtK9_wGfYRJJG_E/prefill';
+  }
 }
